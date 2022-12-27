aa673d9a9b7cf0e4763d6b54a357c9876ea8e4c5
Sonny3fingers <77343092+Sonny3fingers@users.noreply.github.com>
2022-12-27 20:59:23 +0100

    Added home page, hero image frame and responsive styles

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -5,11 +5,13 @@ import Button from "../cta/Button";
 const Hero = () => {
   return (
     <div className={styles.hero}>
-      <div className={styles.heroImg}></div>
+      <div className={styles.frame}>
+        <div className={styles.heroImg}></div>
+      </div>
       <div className={styles.heroText}>
         <h1>Milivoj Jovisevic</h1>
         <h2>Front-End Developer</h2>
-        <p>Anything is posible if you are willing to work for it.</p>
+        <p>Anything is possible if you are willing to work for it.</p>
         <Button />
       </div>
     </div>
